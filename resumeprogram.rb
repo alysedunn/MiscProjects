0a7c290ec1510d488ebbe777ec5cf3b3297ac150
Alyse Dunn <alyse.dunn@gmail.com>
2015-08-30 12:56:09 -0400

    Change hash key value pairs and update methods accordingly to use newly updated hash.

--- resumeprogram.rb
+++ resumeprogram.rb
@@ -7,15 +7,17 @@ $MyResume = ARGV[0]
 
 class Resume 
 
-@@PM_Resume = "Project Management Resume"
-@@Dev_Resume = "Development Resume"
-@@Exit_message = "Exiting Program"
+@@Resume_Hash = {"PM"=> {"Print_Option"=> "Project Management Resume", "File_Name"=> "Alyse Dunn_ProjectManager.txt"},
+				"Dev"=> {"Print_Option"=> "Development Resume", "File_Name"=> "Alyse Dunn_Developer.txt"}
+				}
+
 
 	def Print_instructions 
+		# Add loop for resume name and add readme
 		puts
-		puts "Please enter the number associated with the resume you would like to see. Optons:"
-		puts "1." + " " + @@PM_Resume
-		puts "2." + " " + @@Dev_Resume
+		puts "Please enter 'Dev' if you would like to see the Development resume, or 'PM' if you would like to see the PM resume:"
+		puts "PM" + " " + @@Resume_Hash['PM']["Print_Option"]
+		puts "Dev" + " " + @@Resume_Hash['Dev']["Print_Option"]
 	end
 
 	def Update_user_input_1
@@ -23,42 +25,25 @@ class Resume
 	end
 
 	def Input_1_validation_loop
-		if @user_input_1 == "1" 
-			PM_resume()
-		elsif @user_input_1 == "2"
-			Dev_resume()
+		if @@Resume_Hash.has_key?(@user_input_1) 
+			Resume()
 		else
 			puts "You entered an invalid value."
 		end
 	end
 
-	def PM_resume
-		puts "You selected the"+" "+ @@PM_Resume
+	def Resume
+		puts "You selected the"+" "+ @user_input_1 + "resume"
 		json1 = File.read($MyResume)
 		obj = JSON.parse(json1)
-		@PM_Resume_Just_Created = ""
+		@Resume_Just_Created = ""
 		obj.each do |key, value|
-			value.each do |pm_subkey_key, pm_subkey_value|
-				if pm_subkey_key.include?('PM')
-					puts pm_subkey_value
-					@PM_Resume_Just_Created += pm_subkey_value + "\n"
+			value.each do |subkey_key, subkey_value|
+				if subkey_key.include?(@user_input_1)
+					puts subkey_value
+					@Resume_Just_Created += subkey_value + "\n"
 				end
 			end
-		end
-	end
-
-	def Dev_resume
-		puts "You selected the"+" "+ @@Dev_Resume
-		json1 = File.read($MyResume)
-		obj = JSON.parse(json1)
-		@Dev_Resume_Just_Created = ""
-		obj.each do |key, value|
-			value.each do |dev_subkey_key, dev_subkey_value|
-				if dev_subkey_key.include?('Dev')
-					puts dev_subkey_value
-					@Dev_Resume_Just_Created += dev_subkey_value + "\n"
-	    		end
-	    	end
 		end
 	end
 
@@ -77,7 +62,7 @@ class Resume
 			puts "yes"
 			Create_file()
 		elsif @user_input_2 == "no"
-			puts @@Exit_message
+			puts "Exiting Program"
 		else
 			puts "You entered an invalid value."
 			@user_input_2 = nil
@@ -88,16 +73,9 @@ class Resume
 	end
 
 	def Create_file
-		if @user_input_1 == "1"
-			File.new "Alyse Dunn_ProjectManager.txt","w"
-			File.open("Alyse Dunn_ProjectManager.txt", 'w') { |file| file.write(@PM_Resume_Just_Created) }
-			puts "PM Resume Created"
-		else
-			File.new "Alyse Dunn_Developer.txt","w"
-			File.open("Alyse Dunn_Developer.txt", 'w') { |file| file.write("test") }
-			puts "Dev Resume Created"
-
-		end
+		File.new @@Resume_Hash[@user_input_1]["File_Name"],"w"
+		File.open(@@Resume_Hash[@user_input_1]["File_Name"], "w") { |file| file.write(@Resume_Just_Created) }
+		puts @@Resume_Hash[@user_input_1]["Print_Option"] + "just created"
 	end
 
 end
